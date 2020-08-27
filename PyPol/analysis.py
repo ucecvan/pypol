b4687f974209f84761e5e8f7665b3fce32ca384d
nicholasfrancia <ucecnff@ucl.ac.uk>
2020-08-27 12:14:45 +0100

    Update-2020.08.27

--- PyPol/analysis.py
+++ PyPol/analysis.py
@@ -591,10 +591,11 @@ class Combine(object):
 
     def __init__(self, name, method, cvs):
         self.name = name
-        self.type = "N-Dimensional CV"
+        self.cvs = cvs
+
+        self.type = "{} ({}D)".format(cvs[0].type, len(cvs))
         self.clustering_type = "distribution"
         self.method = method
-        self.cvs = cvs
 
         self.kernel = cvs[0].kernel
         self.timeinterval = cvs[0].timeinterval
@@ -681,40 +682,67 @@ class Combine(object):
             grid_bin += "{},".format(cv.grid_bin)
             list_bins.append(int(cv.grid_bin))
             bandwidth += "{:.3f},".format(cv.bandwidth)
-            args += "ARG{}=ang_mat_{} ".format(idx_cv + 1, cv.name)
+            if self.type.startswith("Molecular Orientation"):
+                args += "ARG{}=ang_mat_{} ".format(idx_cv + 1, cv.name)
+            elif self.type.startswith("Torsional Angle"):
+                args += "ARG{}={} ".format(idx_cv + 1, cv.name)
+
             idx_cv += 1
         self.list_bins = tuple(list_bins)
         print("\nClustering type: {5}-D Distribution\n"
               "Parameters: KERNEL={0} NBINS={1} BANDWIDTH={2} UPPER={3} LOWER={4}"
               "".format(self.kernel, grid_bin, bandwidth, grid_max, grid_min, len(self.cvs)))
 
-        for crystal in simulation.crystals:
-            print(crystal.name)
-            file_plumed = open(crystal.path + "plumed_" + self.name + ".dat", "w")
-            for cv in self.cvs:
-                # Select atoms and molecules
-                lines_atoms = []
-                for idx_mol in range(len(cv.molecules)):
-                    lines_atoms = generate_atom_list(cv.atoms[idx_mol], cv.molecules[idx_mol], crystal,
-                                                     keyword="ATOMS", lines=lines_atoms)
+        if self.type.startswith("Molecular Orientation"):
+            for crystal in simulation.crystals:
+                print(crystal.name)
+                file_plumed = open(crystal.path + "plumed_" + self.name + ".dat", "w")
+                for cv in self.cvs:
+                    # Select atoms and molecules
+                    lines_atoms = []
+                    for idx_mol in range(len(cv.molecules)):
+                        lines_atoms = generate_atom_list(cv.atoms[idx_mol], cv.molecules[idx_mol], crystal,
+                                                         keyword="ATOMS", lines=lines_atoms)
 
-                file_plumed.write("DISTANCE ...\n")
-                for line in lines_atoms:
-                    file_plumed.write(line)
-                file_plumed.write("LABEL=dd_{0}\n"
-                                  "COMPONENTS\n"
-                                  "... DISTANCE\n\n"
-                                  "vv_{0}: NORMALIZE ARG1=dd_{0}.x ARG2=dd_{0}.y ARG3=dd_{0}.z\n"
-                                  "dp_mat_{0}: DOTPRODUCT_MATRIX GROUP1=vv_{0}.x GROUP2=vv_{0}.y GROUP3=vv_{0}.z\n"
-                                  "ang_mat_{0}: MATHEVAL ARG1=dp_mat_{0} FUNC=acos(x) PERIODIC=NO\n\n"
-                                  "".format(cv.name))
+                    file_plumed.write("DISTANCE ...\n")
+                    for line in lines_atoms:
+                        file_plumed.write(line)
+                    file_plumed.write("LABEL=dd_{0}\n"
+                                      "COMPONENTS\n"
+                                      "... DISTANCE\n\n"
+                                      "vv_{0}: NORMALIZE ARG1=dd_{0}.x ARG2=dd_{0}.y ARG3=dd_{0}.z\n"
+                                      "dp_mat_{0}: DOTPRODUCT_MATRIX GROUP1=vv_{0}.x GROUP2=vv_{0}.y GROUP3=vv_{0}.z\n"
+                                      "ang_mat_{0}: MATHEVAL ARG1=dp_mat_{0} FUNC=acos(x) PERIODIC=NO\n\n"
+                                      "".format(cv.name))
 
-            file_plumed.write("valg_{0}: KDE {7} GRID_MIN={1} GRID_MAX={2} "
-                              "GRID_BIN={3} BANDWIDTH={4} KERNEL={5}\n\n"
-                              "PRINT ARG=valg_{0} FILE=plumed_{6}_{0}.dat\n"
-                              "".format(self.name, grid_min, grid_max,
-                                        grid_bin, bandwidth, self.kernel, simulation.name, args))
-            file_plumed.close()
+                file_plumed.write("valg_{0}: KDE {7} GRID_MIN={1} GRID_MAX={2} "
+                                  "GRID_BIN={3} BANDWIDTH={4} KERNEL={5}\n\n"
+                                  "PRINT ARG=valg_{0} FILE=plumed_{6}_{0}.dat\n"
+                                  "".format(self.name, grid_min, grid_max,
+                                            grid_bin, bandwidth, self.kernel, simulation.name, args))
+                file_plumed.close()
+
+        if self.type.startswith("Torsional Angle"):
+            for crystal in simulation.crystals:
+                print(crystal.name)
+                file_plumed = open(crystal.path + "plumed_" + self.name + ".dat", "w")
+                for cv in self.cvs:
+                    # Select atoms and molecules
+                    lines_atoms = []
+                    for idx_mol in range(len(cv.molecules)):
+                        lines_atoms = generate_atom_list(cv.atoms[idx_mol], cv.molecules[idx_mol], crystal,
+                                                         keyword="ATOMS", lines=lines_atoms)
+                    file_plumed.write("TORSIONS ...\n")
+                    for line in lines_atoms:
+                        file_plumed.write(line)
+
+                    file_plumed.write("LABEL={0}\n... TORSIONS\n\n".format(cv.name))
+                file_plumed.write("valg_{0}: KDE {7} GRID_MIN={1} GRID_MAX={2} "
+                                  "GRID_BIN={3} BANDWIDTH={4} KERNEL={5}\n\n"
+                                  "PRINT ARG=valg_{0} FILE=plumed_{6}_{0}.dat\n"
+                                  "".format(self.name, grid_min, grid_max,
+                                            grid_bin, bandwidth, self.kernel, simulation.name, args))
+                file_plumed.close()
 
         if bash_script:
             dt, nsteps, traj_stride, traj_start, traj_end = (None, None, None, None, None)
@@ -787,7 +815,9 @@ class Combine(object):
                     np.savetxt(crystal.path + "plumed_{}_{}_data.dat".format(simulation.name, self.name),
                                crystal.cvs[self.name],
                                header="Probability Density Grid.")
-                    plt.imshow(crystal.cvs[self.name], interpolation="nearest", cmap="viridis")
+                    extent = self.cvs[0].grid_min, self.cvs[0].grid_max, self.cvs[1].grid_min, self.cvs[1].grid_max
+
+                    plt.imshow(crystal.cvs[self.name], interpolation="nearest", cmap="viridis", extent=extent)
                     plt.xlabel("{} / rad".format(self.cvs[0].name))
                     plt.ylabel("{} / rad".format(self.cvs[1].name))
                     plt.savefig(crystal.path + "plumed_{}_{}_plot.png".format(simulation.name, self.name), dpi=300)
